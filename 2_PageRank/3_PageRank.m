e8131415e80aa0243e85e8f19a559c6859d87795
ash <81823132+ash0920-git@users.noreply.github.com>
2025-04-20 14:55:11 +0800

    Update 3_PageRank.m

--- 2_PageRank/3_PageRank.m
+++ 2_PageRank/3_PageRank.m
@@ -1,7 +1,7 @@
 clc ;
 clear;
-s = xlsread('wl.xlsx','wl','A1:A24245'); 
-t = xlsread('wl.xlsx','wl','B1:B24245'); 
+s = xlsread('wl.xlsx','wl','A1:A79889'); 
+t = xlsread('wl.xlsx','wl','B1:B79889'); 
 %names = {'1', '2', '3', '4', '5', '6'};
 G = digraph(s,t,[]);
 plot(G)
